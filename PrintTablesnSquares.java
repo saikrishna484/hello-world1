6b31b6aaf8f0c9ec04000200a1f181e8bf2f90b2
saikrishna484 <saikrishna484@gmail.com>
2021-09-09 16:20:26 +0800

    first commit

---
+++ PrintTablesnSquares.java
@@ -0,0 +1,20 @@
+class PrintTablesnSquares{
+void printTable(int number){
+for(int j=1;j<=number;j++){
+for (int i=1;i<=10;i++){
+System.out.printf(" %d * %d = %d", j , i , j*i).println();
+}
+System.out.println("\n");
+}
+}
+void printSquare(number){
+for(int k=1;k<=number;k++){
+System.out.println(k*k);
+}
+}
+public static void main(String[] args){
+PrintTablesnSquares PrintTables1nSquares1= new PrintTablesnSquares():
+PrintTables1nSquares1.printTable(5);
+PrintTables1nSquares1.printSquare(5);
+}
+}
